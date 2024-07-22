288954280647ac0442f198ee07a1545110776525
rlawh <rlawhdduq95@gmail.com>
2024-07-23 06:41:51 +0900

    배포를 위한 쉘 스크립트 추가(수정2)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=freelec-springboot2-webservice
+PROJECT_NAME=freelec2-springboot2-websrevice
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
