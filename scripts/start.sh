53da16b2e2ca675d875df294ef971b482151d6c9
rlawh <rlawhdduq95@gmail.com>
2024-07-23 20:39:06 +0900

    제 10장 무중단 배포(idle_profile 로그 찍어보기

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,7 +16,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행 : idle_profile: $IDLE_PROFILE"
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
